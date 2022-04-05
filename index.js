cae88d9a67c1d2b317cf0bd2484610148375459e
pavlo <pavel.shupenko@gmail.com>
2022-04-05 21:14:43 +0300

    feat: add task3.1

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
+const name = prompt('Как вас зовут?')
+const secondName = prompt('Какая у вас фамилия?')
+const middleName = prompt('Какое у вас отчество?')
+const age = prompt('Сколько вам лет?')
+let sex = confirm('Ваш пол - мужской?')
 
-function func1() {
-    let color = document.getElementById("col").value
-    document.body.style.backgroundColor = `${color}`
-}
 
-function func2() {
-    let color = document.querySelector(".selector").value
-    
-    document.body.style.backgroundColor = `${color}`
-}
+if (sex === true)
+    sex = 'Мужской'
+else
+    sex = 'Женский'
 
-function func3() {
-    let color = document.body.querySelector(".selector2").value
-    document.body.className = `${color}`
-    document.body.style.backgroundColor = `${document.body.className }`;
-}
+const ageInDay = age * 365
+const ageInFive = Number(age) + 5
 
-function func4() {
-    let color = document.body.querySelector(".input").value
-    document.body.style.setProperty('--main-color',`${color}`);
-}
+if (age < 63)
+    pension = "Нет"
+else
+    pension = "Да"
 
-function func5(selectElement) {
-    document.body.style.setProperty('--main-color', `${selectElement.value}`)
-}
+alert(`Ваше ФИО: ${name} ${secondName} ${middleName}\nВаш возраст в годах: ${age}\nВаш возраст в днях:
+     ${ageInDay}\nЧерез 5 лет вам будет: ${ageInFive}\nВаш пол: ${sex}\nВы на пенсии: ${pension}`)
