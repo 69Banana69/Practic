6a5ce8f32ea74b892f7c72751ba68f5d1c94eb71
pavlo <pavel.shupenko@gmail.com>
2022-04-05 15:00:49 +0300

    feat: task 2.2

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 
-
-
-function func() {
+function func1() {
     let color = document.getElementById("col").value
+    document.body.style.backgroundColor = `${color}`
+}
+
+function func2() {
+    let color = document.querySelector(".selector").value
+    
     document.body.style.backgroundColor = `${color}`
 }
