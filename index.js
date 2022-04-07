252171938fddcaef64b3d3f69ef50c284589685b
pavlo <pavel.shupenko@gmail.com>
2022-04-07 11:55:27 +0300

    feat: add task3.2

--- index.js
+++ index.js
@@ -1,57 +1,13 @@
-convert()
-function convert() {
-    const name = prompt('Как вас зовут?')
-    const secondName = prompt('Какая у вас фамилия?')
-    const middleName = prompt('Какое у вас отчество?')
-    const age = prompt('Сколько вам лет?')
-    let sex = confirm('Ваш пол - мужской?')
-    const ageInDay = age * 365
-    const ageInFive = Number(age) + 5
-    let err = []
-
-    if (sex === true) {
-        sex = 'Мужской'
-    } else {
-        sex = 'Женский'
-    }
-
-    if (sex === 'Мужской') {
-        if (age < 60) {
-            pension = 'Нет'
-        } else {
-            pension = 'Да'
-        }
-    } else {
-        if (age < 55) {
-            pension = 'Нет'
-        } else {
-            pension = 'Да'
-        }
-    }
-    if ((isNaN(Number(age)) == true) || (age === '') || (age > 100)) {
-        err += ['Неверный возраст\n']
-    }
-
-    if (name == '') {
-        err += ['Неверное имя\n']
-    }
-
-    if (secondName == '') {
-        err += ['Неверная фамилия\n']
-    }
-
-    if (middleName == '') {
-        err += ['Неверное отчество\n']
-    }
-    console.log(err)
-    if (err.length === 0) {
-        alert(`Ваше ФИО: ${secondName} ${name} ${middleName}\nВаш возраст в годах: ${age}\nВаш возраст в днях: ${ageInDay}\nЧерез 5 лет вам будет: ${ageInFive}\nВаш пол: ${sex}\nВы на пенсии: ${pension}`)
-
-    } else {
-        alert(err)
-        reload = confirm('Попробуете еще раз?')
-        if (reload === true) {
-            convert()
-        }
-    }
+let audience = {
+    number: 57,
+    furniture: ['table', 'chair', 'closet'],
+    board: {
+        material: 'Wood',
+        size: '100x200',
+    },
+    computer: true,
+    projector: false,
+    flower: 5,
+    floor: 'Wood',
+    wall: 'concrete',
 }
