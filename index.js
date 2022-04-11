b4e7ae984284258a0578cca20a558b9e18101739
pavlo <pavel.shupenko@gmail.com>
2022-04-11 14:21:00 +0300

    fix: fix code

--- index.js
+++ index.js
@@ -7,13 +7,9 @@ function convert() {
     let sex = confirm('Ваш пол - мужской?')
     const ageInDay = age * 365
     const ageInFive = Number(age) + 5
-    let err = []
+    let err = ''
 
-    if (sex === true) {
-        sex = 'Мужской'
-    } else {
-        sex = 'Женский'
-    }
+    sex = sex ? 'Мужской' : 'Женский'
 
     if (sex === 'Мужской') {
         if (age < 60) {
@@ -28,28 +24,28 @@ function convert() {
             pension = 'Да'
         }
     }
-    if ((isNaN(Number(age)) == true) || (age === '') || (age > 100)) {
-        err += ['Неверный возраст\n']
+    if ((isNaN(Number(age)) === true) || (!age) || (age > 100)) {
+        err += 'Неверный возраст\n'
     }
 
-    if (name == '') {
-        err += ['Неверное имя\n']
+    if(!name) {
+        err += 'Неверное имя\n'
     }
 
-    if (secondName == '') {
-        err += ['Неверная фамилия\n']
+    if (!secondName) {
+        err += 'Неверная фамилия\n'
     }
 
-    if (middleName == '') {
-        err += ['Неверное отчество\n']
+    if (!middleName) {
+        err += 'Неверное отчество\n'
     }
-    console.log(err)
-    if (err.length === 0) {
+
+    if (!err.length) {
         alert(`Ваше ФИО: ${secondName} ${name} ${middleName}\nВаш возраст в годах: ${age}\nВаш возраст в днях: ${ageInDay}\nЧерез 5 лет вам будет: ${ageInFive}\nВаш пол: ${sex}\nВы на пенсии: ${pension}`)
 
     } else {
         alert(err)
-        reload = confirm('Попробуете еще раз?')
+        const reload = confirm('Попробуете еще раз?')
         if (reload === true) {
             convert()
         }
