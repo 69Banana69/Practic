dfc124c66a544271aaff8f5b3e45bbfe1eff3db7
pavlo <pavel.shupenko@gmail.com>
2022-04-15 13:56:20 +0300

    fix: fix additem

--- main.js
+++ main.js
@@ -18,7 +18,7 @@ function init() {
     textColorInput: document.querySelector("input.text-color-input"),
     addButton: document.querySelector("button.add-button"),
     items: document.querySelector("div.items"),
-    removeButton: document.querySelectorAll("button.delete-button"),
+    removeButton: document.querySelectorAll(".delete-button"),
   };
   render();
   removeItem();
@@ -31,6 +31,7 @@ function render() {
     let div = document.createElement("div");
     div.className = "item";
     div.style.backgroundColor = itemsArray[i].bgColor;
+    div.id = itemsArray[i].color;
     itms.append(div);
 
     let span = document.createElement("span");
@@ -48,23 +49,37 @@ function render() {
 
 function removeItem() {
   let btns = document.querySelectorAll(".delete-button");
+  let colors = itemsArray.map((el) => el.color);
 
   btns.forEach((btn) => btn.addEventListener("click", removeItems));
-  function removeItems() {
-    this.parentNode.remove();
 
-    render();
+  function removeItems() {
+   
+    let index = colors.indexOf(`${this.parentNode.id}`);
+    itemsArray.splice(index, 1);
+    this.parentNode.remove();
   }
 }
 
 function selectItem() {
   let item = document.querySelectorAll(".item");
+  let btns = document.querySelectorAll(".delete-button");
 
   item.forEach((items) => items.addEventListener("click", selectItems));
+  btns.forEach((btn) =>
+    btn.addEventListener("click", function selectBtn(event) {
+      event.stopPropagation();
+    })
+  );
   function selectItems() {
-    window.getSelection().selectAllChildren(this);
+    document.querySelector(".header").style.backgroundColor =
+      this.style.backgroundColor;
+    document.querySelector(".header").style.color =
+      this.firstChild.style.color;
   }
+  
 }
+
 
 function addItem() {
   itemsArray.push({
@@ -74,6 +89,7 @@ function addItem() {
   lastChild = itemsArray.slice(-1);
   let div = document.createElement("div");
   div.className = "item";
+  div.id = lastChild[0].bgColor
   div.style.backgroundColor = lastChild[0].bgColor;
   itms.append(div);
 
@@ -87,4 +103,7 @@ function addItem() {
   btn.className = "delete-button";
   btn.textContent = "X";
   div.appendChild(btn);
+  removeItem()
+  selectItem()
+
 }
